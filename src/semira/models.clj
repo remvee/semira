cdfaf7cb35b0e93d3a39f8a52db6f16cde6cee2c
Remco van 't Veer <rwvtveer@xs4all.nl>
2011-03-05 13:45:57 +0100

    cleanup update functions

--- src/semira/models.clj
+++ src/semira/models.clj
@@ -6,64 +6,64 @@
 
 (defn albums [] @albums-container)
 
-(defn- sha1 [value]
+(defn- sha1
+  "Calculate SHA1 digest for given string."
+  [string]
   (let [digest (java.security.MessageDigest/getInstance "SHA1")
-        value  (java.io.StringBufferInputStream. value)
-        output (java.security.DigestInputStream. value digest)]
+        string  (java.io.StringBufferInputStream. string)
+        output (java.security.DigestInputStream. string digest)]
     (while (not= -1 (.read output)))
     (apply str (map #(format "%02x" %)
                     (.digest digest)))))
 
+(defn- sort-by-keys
+  "Sort coll of maps by value with ks defining precedence."
+  [coll & ks]
+  (sort-by (fn [val]
+             (vec (map #(get val %)
+                       ks)))
+           coll))
+
 (defn update-album [album]
+  (swap! albums-container
+         (fn [albums album]
+           (sort-by-keys (conj (filter #(not= (:id album)
+                                              (:id %))
+                                       albums)
+                               album)
+                         :artist :year :album))
+         album))
+
+(defn normalize-album [album]
   (let [tracks (map #(merge album %) (:tracks album))
-        common (filter #(apply = (map % tracks))
-                       (into #{} (flatten (map keys tracks))))
-        album (merge
-               (select-keys (first tracks) common)
-               {:tracks (sort (fn [a b]
-                                (cond ; TODO make prettier
-                                 (and (:track a) (:track b))
-                                 (.compareTo (:track a) (:track b))
+        common (filter #(and (not= :id %)
+                             (apply = (map % tracks)))
+                       (into #{} (flatten (map keys tracks))))]
+    (merge
+     (select-keys (first tracks) common)
+     {:id (:id album)
+      :tracks (vec (sort-by-keys (map #(apply dissoc % common)
+                                      tracks)
+                                 :track :path :title))})))
 
-                                 (and (:path a) (:path b))
-                                 (.compareTo (:path a) (:path b))
-
-                                 :else 0))
-                              (map #(apply dissoc % common) tracks))}
-               {:id (sha1 (:dir (first tracks)))})]
-    (swap! albums-container
-           (fn [albums album]
-             (sort (fn [a b]
-                     (.compareTo (apply str (map #(% a) [:artist :year :album]))
-                                 (apply str (map #(% b) [:artist :year :album]))))
-                   (conj (filter #(not= (:dir album)
-                                        (:dir %))
-                                 albums)
-                         album))) album)))
-
-(defn update-track [track] ; TODO should happen in update-album swap!
-  (let [dir (-> (:path track) File. .getParent)
-        cur (first (filter #(= dir (:dir %))
-                           (albums)))]
-    (update-album
-     (if cur
-       (update-in cur [:tracks]
-                  (fn [tracks track]
-                    (conj (vec (filter #(not= (:path track)
-                                              (:path %)) tracks))
-                          track)) track)
-       (into {:dir dir
-              :tracks [track]}
-             (filter last
-                     {:dir dir
-                      :album (:album track)
-                      :year (:year track)
-                      :genre (:genre track)}))))))
+(defn update-track [track]
+  (let [id (sha1 (:dir track))
+        album (update-in (or (album-by-id id)
+                             {:id id
+                              :tracks []})
+                         [:tracks]
+                         (fn [tracks]
+                           (conj (vec (filter #(not= (:id track)
+                                                     (:id %))
+                                              tracks))
+                                 track)))]
+    (update-album (normalize-album album))))
 
 (defn update-file [file]
   (update-track (merge (audio/info file)
-                       {:path (.getPath file)
-                        :id (sha1 (.getPath file))})))
+                       {:id (sha1 (.getPath file))
+                        :dir (.getParent file)
+                        :path (.getPath file)})))
 
 (defn album-by-id [id]
   (first (filter #(= id (:id %))
