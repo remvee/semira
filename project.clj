21f57cf7c5264090cd3b7c45f6a471d51c69305f
Remco van 't Veer <rwvtveer@xs4all.nl>
2011-03-06 09:54:15 +0100

    upgrade ring compojure and hiccup

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Semira sings songs."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.6.0"]
-                 [hiccup "0.3.1"]
-                 [ring/ring-jetty-adapter "0.3.5"]
-                 [ring/ring-devel "0.3.5"]
+                 [compojure "0.6.1"]
+                 [hiccup "0.3.4"]
+                 [ring/ring-jetty-adapter "0.3.6"]
+                 [ring/ring-devel "0.3.6"]
                  [org/jaudiotagger "2.0.3"]])
