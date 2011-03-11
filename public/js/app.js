be3e5fb320e4eba7d5fdfc24caf3b10dbc952d58
Remco van 't Veer <rwvtveer@xs4all.nl>
2011-03-11 14:21:33 +0100

    ogg and mp3 sources for audio tag

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,25 @@ $(document).ready(function() {
   $("li.track a").click(function(event) {
     if (!!(document.createElement('audio').canPlayType)) {
       var container = document.getElementById("audio-container");
-      var audio = document.createElement("audio");
-
       while (container.firstChild) container.removeChild(container.firstChild);
 
-      audio.src = this.getAttribute("href");
+      var mp3 = document.createElement("source");
+      mp3.src = this.getAttribute("href-mp3");
+      mp3.type = "audio/mpeg";
+
+      var ogg = document.createElement("source");
+      ogg.src = this.getAttribute("href-ogg");
+      ogg.type = "audio/ogg";
+
+      var audio = document.createElement("audio");
       audio.autoplay = "autoplay";
       audio.controls = "controls";
+      audio.preload = "none";
+      audio.appendChild(mp3);
+      audio.appendChild(ogg);
+
       container.appendChild(audio);
+
       event.preventDefault();
     }
   })
