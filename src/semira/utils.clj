9db0effdf1379a3856882987a1d180e018c7478e
Remco van 't Veer <rwvtveer@xs4all.nl>
2011-03-18 14:45:03 +0100

    apply with-open to sha1 java mess

--- src/semira/utils.clj
+++ src/semira/utils.clj
@@ -1,17 +1,17 @@
 (ns semira.utils
   (:require [clojure.string :as string])
-  (:import [java.io File]
-           [java.util.regex Pattern]))
+  (:import [java.io File StringBufferInputStream]
+           [java.util.regex Pattern]
+           [java.security MessageDigest DigestInputStream]))
 
 (defn sha1
   "Calculate SHA1 digest for given string."
   [string]
-  (let [digest (java.security.MessageDigest/getInstance "SHA1")
-        string  (java.io.StringBufferInputStream. string)
-        output (java.security.DigestInputStream. string digest)]
-    (while (not= -1 (.read output)))
-    (apply str (map #(format "%02x" %)
-                    (.digest digest)))))
+  (let [digest (MessageDigest/getInstance "SHA1")]
+    (with-open [sbin  (StringBufferInputStream. string)]
+      (with-open [din (DigestInputStream. sbin digest)]
+        (while (pos? (.read din))))
+      (apply str (map #(format "%02x" %) (.digest digest))))))
 
 (defn sort-by-keys
   "Sort coll of maps by value with ks defining precedence."
