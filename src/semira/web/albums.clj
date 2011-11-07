731e666c186f3c9f71489ae4b0894aec8beb567b
Remco van 't Veer <remco@remworks.net>
2011-11-07 10:38:47 +0100

    list new addition when query given

--- src/semira/web/albums.clj
+++ src/semira/web/albums.clj
@@ -32,11 +32,13 @@
                  (let [offset (if offset (Integer/valueOf offset) 0)
                        limit (if limit (Integer/valueOf limit) utils/*page-size*)
                        keys (if keys (map keyword keys) [:genre :artist :album :year :id])
+                       sorter (if (empty? query)
+                                #(reverse (sort-by :mtime %))
+                                #(utils/sort-by-keys % keys))
                        albums (take limit
                                     (drop offset
                                           (map #(select-keys % keys)
-                                               (utils/sort-by-keys (models/albums {:query query})
-                                                                   keys))))]
+                                               (sorter (models/albums {:query query})))))]
                    {:status 200
                     :headers {"Content-Type" "application/clojure; charset=utf-8"}
                     :body (pr-str albums)}))
