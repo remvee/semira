8e8c072a29245417cf33a9fafa75193a4bf293f3
Remco van 't Veer <remco@remworks.net>
2018-11-03 14:00:02 +0100

    Drop compass dependency

--- project.clj
+++ project.clj
@@ -14,27 +14,37 @@
                  [reagent "0.7.0"]]
 
   :plugins [[lein-cljsbuild "1.1.3"]
-            [lein-figwheel "0.5.3-2"]]
+            [lein-figwheel "0.5.3-2"]
+            [deraen/lein-sass4clj "0.3.1"]
+            [lein-exec "0.3.7"]]
 
   :figwheel {:css-dirs ["generated/public"]}
 
-  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.14"]
-                                  [com.cemerick/piggieback "0.2.2"]
-                                  [ring/ring-mock "0.3.1"]]}
-             :uberjar {:aot :all
-                       :hooks [leiningen.cljsbuild]}}
+  :profiles {:dev     {:dependencies [[figwheel-sidecar "0.5.14"]
+                                      [com.cemerick/piggieback "0.2.2"]
+                                      [ring/ring-mock "0.3.1"]]}
+             :uberjar {:aot        :all
+                       :prep-tasks ["compile-sass" "compile" ["cljsbuild" "once" "prod"]]}}
+
   :main semira.core
   :uberjar-name "semira.jar"
   :resource-paths ["resources" "generated"]
 
-  :cljsbuild {:builds {:prod {:source-paths ["src"]
-                              :compiler {:optimizations :advanced
-                                         :output-to "generated/public/semira.js"
-                                         :output-dir "generated/public/semira"}}
-                       :dev {:source-paths ["src"]
-                             :figwheel true
-                             :compiler {:output-to "generated/public/semira-dev.js"
-                                        :output-dir "generated/public/semira-dev"}}}}
+  :sass {:target-path  "generated/public"
+         :source-paths ["sass"]
+         :source-map   true}
 
-  :repl-options {:init (do (use 'figwheel-sidecar.repl-api) (start-figwheel!))
+  :aliases { "compile-sass" ["exec" "-e" ;; https://github.com/Deraen/sass4clj/issues/18#issuecomment-412299327
+                             "(println (:out (clojure.java.shell/sh \"lein\" \"sass4clj\" \"once\")))"]}
+
+  :cljsbuild {:builds {:prod {:source-paths ["src"]
+                              :compiler     {:optimizations :advanced
+                                             :output-to     "generated/public/semira.js"
+                                             :output-dir    "generated/public/semira"}}
+                       :dev  {:source-paths ["src"]
+                              :figwheel     true
+                              :compiler     {:output-to  "generated/public/semira-dev.js"
+                                             :output-dir "generated/public/semira-dev"}}}}
+
+  :repl-options {:init    (do (use 'figwheel-sidecar.repl-api) (start-figwheel!))
                  :init-ns semira.core})
